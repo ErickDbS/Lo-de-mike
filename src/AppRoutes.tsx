1434974cb4c646465a5767c9f1a4dc0a9eb987e3
Erick Bojorquez <erickdelmedio@hotmail.com>
2025-03-17 14:24:57 -0700

    Initial commit

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./Components/Login-Register/Login";
+import Register from "./Components/Login-Register/Register";
+import Inicio from "./Inicio";
+
+export default function AppRoutes(){
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Inicio />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </>
+    )
+}
