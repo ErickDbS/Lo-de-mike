460d40c8c0d7005b11e5977ebad27e1da87581ee
Christian Lugo <christianlugo1503@gmail.com>
2025-05-08 16:30:48 -0700

    Cambios escenciales en el navbar

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,27 +1,42 @@
 interface CardProps {
     nameProfesor: string;
     nameMateria: string;
-    Hora:string;
+    Hora: string;
 }
 
-export default function Card({nameProfesor, nameMateria, Hora}: CardProps){
-    
+export default function Card({ nameProfesor, nameMateria, Hora }: CardProps) {
     return (
         <>
-
-        <div className="grid place-items-center">
-            <div className="text-white text-4xl border border-solid rounded-xl w-100 grid place-items-center ">
-                <div className="text-xl pt-5">
-                    <p><strong className="pt-1 text-blue-800">Materia:</strong> {nameProfesor}</p>
-                    <p><strong className="pt-1 text-blue-800">Profesor: </strong> {nameMateria}</p>
-                    <p><strong className="pt-1 text-blue-800">Hora: </strong> {Hora}</p> 
+            <div className="grid place-items-center">
+                <div className="text-white text-4xl border border-solid border-gray-300/20  rounded-xl w-100 grid place-items-center ">
+                    <div className="text-xl pt-5">
+                        <p>
+                            <strong className="pt-1 text-blue-500">
+                                Materia:
+                            </strong>{" "}
+                            {nameProfesor}
+                        </p>
+                        <p>
+                            <strong className="pt-1 text-blue-500">
+                                Profesor:{" "}
+                            </strong>{" "}
+                            {nameMateria}
+                        </p>
+                        <p>
+                            <strong className="pt-1 text-blue-500">
+                                Hora:{" "}
+                            </strong>{" "}
+                            {Hora}
+                        </p>
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-blue-800 px-5 py-4 mt-3 mb-3 rounded-xl cursor-pointer text-xl hover:bg-blue-900 transition"
+                    >
+                        Marcar Asistencia
+                    </button>
                 </div>
-                <button type="submit" className="bg-blue-800 p-5 mt-3 mb-3 rounded-xl cursor-pointer text-xl hover:bg-blue-900 transition">
-                    Marcar Asistencia
-                </button>
             </div>
-        </div>
-
         </>
-    )
+    );
 }
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -3,14 +3,38 @@ import Schedule from "../../components/schedule/Schedule";
 
 export default function Home() {
     const horarios = [
-        {hora: "17:00 - 18:00", materia: "la de Edgar", profesor: "El Edgar", tema: "el que da Edgar"},
-        {hora: "18:00 - 19:00", materia: "la de Mike", profesor: "El Mike", tema: "el que da Mike"},
-        {hora: "19:00 - 20:00", materia: "la de Herman", profesor: "El Herman", tema: "el que da Herman"},
-        {hora: "20:00 - 21:00", materia: "la de Mirsa", profesor: "El Mirsa", tema: "el que da Mirsa"},
-        {hora: "21:00 - 22:00", materia: "la de Rocio", profesor: "La Rocio", tema: "la que da Rocio"},
-    ]
+        {
+            hora: "17:00 - 18:00",
+            materia: "la de Edgar",
+            profesor: "El Edgar",
+            tema: "el que da Edgar",
+        },
+        {
+            hora: "18:00 - 19:00",
+            materia: "la de Mike",
+            profesor: "El Mike",
+            tema: "el que da Mike",
+        },
+        {
+            hora: "19:00 - 20:00",
+            materia: "la de Herman",
+            profesor: "El Herman",
+            tema: "el que da Herman",
+        },
+        {
+            hora: "20:00 - 21:00",
+            materia: "la de Mirsa",
+            profesor: "El Mirsa",
+            tema: "el que da Mirsa",
+        },
+        {
+            hora: "21:00 - 22:00",
+            materia: "la de Rocio",
+            profesor: "La Rocio",
+            tema: "la que da Rocio",
+        },
+    ];
     return (
-
         <div className="flex flex-col items-center justify-center">
             {/* Se mostrara el horario ya sea del maestro o alumno, con la fila resaltada de la hora actual de clases */}
             <div className="w-full max-w-5xl flex items-center justify-center rounded text-white">
@@ -18,26 +42,16 @@ export default function Home() {
                     grupo="4-03-IS"
                     aula="Aula 20"
                     horarios={horarios}
-                >
-                    
-                </Schedule>
+                ></Schedule>
             </div>
 
             {/* Se mostrara la hora actual, junto con la materia y profesor que toque y habra un campo para marcar su falta, o asistencia, cabe recalcar que la falta solo se marcara si pasan los minutos de tolerancia */}
-            <p className="text-gray-300 mt-10">
-                Se mostrara la hora actual, junto con la materia y profesor que
-                toque y habra un campo para marcar su falta, o asistencia, cabe
-                recalcar que la falta solo se marcara si pasan los minutos de
-                tolerancia{"  "}
-            </p>
             <div className="mt-10 bg-[#1e2022] flex flex-col items-center justify-center rounded text-white">
-                <Card 
-                nameProfesor="Edgar Perez"
-                nameMateria="Redes Neuronales"
-                Hora="17:00 - 18:00"
-                >
-
-                </Card>
+                <Card
+                    nameProfesor="Edgar Perez"
+                    nameMateria="Redes Neuronales"
+                    Hora="17:00 - 18:00"
+                ></Card>
             </div>
         </div>
     );
